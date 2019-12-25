0882368f41a3f6033e5ac2aa2f226239b107248b
haeram27 <haeram27@gmail.com>
2019-12-25 22:33:48 +0900

    chrono test

---
+++ main/src/main.cc
@@ -0,0 +1,13 @@
+#include <chrono>
+#include <iostream>
+#include <string>
+#include "chronotest.h"
+
+using namespace std::chrono;
+
+int main() {
+
+	chronotest();
+
+    return 0;
+}
---
+++ main/src/test/chronotest.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+long fibonacci(unsigned n);
+void GetCurrentTimeString(std::string& current_time, std::string& rfc3339_time);
+int chronotest();
